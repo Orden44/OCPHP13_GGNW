ac15c025e60dfd9e3cad8304f2cfef3b48e554fe
Laurent Fenu <laurentfenu.devweb@gmail.com>
2024-09-29 14:58:49 +0200

    create home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    public function __construct(private ProductRepository $productRepository)
+    {   
+    }
+
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $products = $this->productRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $products,
         ]);
     }
 }
