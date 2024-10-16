ca0bc5ed1e8ae0b8d46d80819ed880e9e8fa8ff7
Laurent Fenu <laurentfenu.devweb@gmail.com>
2024-10-16 13:05:45 +0200

    ApiLoginController and ApiProductController changes

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -14,7 +14,6 @@ use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
 
-
 class ApiLoginController extends AbstractController
 {
     private $jwtEncoder;
@@ -26,7 +25,7 @@ class ApiLoginController extends AbstractController
 
     #[Route('/api/login', name: 'app_api_login', methods: ['POST'])]
     #[OA\Post(
-        path: "api/login",
+        path: "/api/login",
         summary: "Permet d'obtenir le token JWT pour se logger.",
         description: "Crée un nouveau token JWT",
         requestBody: new OA\RequestBody(
@@ -34,41 +33,29 @@ class ApiLoginController extends AbstractController
             content: new OA\JsonContent(
                 type: "object",
                 properties: [
-                    new OA\Property(property: "username", type: "string", default: "orden@gmail.com"),
-                    new OA\Property(property: "password", type: "string", default: "Motdepasse")
+                    new OA\Property(property: "username", type: "string", default: "user@gmail.com"),
+                    new OA\Property(property: "password", type: "string", default: "password"),
                 ]
             )
         ),
-        responses: [
-            new OA\Response(
-                response: 200,
-                description: 'Récupère le token JWT',
-                content: new OA\JsonContent(
-                    type: "object",
-                    properties: [
-                        new OA\Property(property: "token", type: "string", example: "your_jwt_token_here")
-                    ]
-                )
-            ),
-            new OA\Response(response: 401, description: "Invalid credentials"),
-            new OA\Response(response: 400, description: "Bad Request")
-        ],
-        tags: ["Login"]
     )]
     #[Security(name: null)]
     #[OA\Response(
         response: 200,
-        description: 'Récupère le token JWT'
+        description: 'Récupère le token JWT',
     )]
     #[OA\Tag(name: 'Login')]
-    public function login(SerializerInterface $serializer, Request $request, UserRepository $userRepository): Response 
-    {
+    public function apiLogin(SerializerInterface $serializer, Request $request, UserRepository $userRepository): Response 
+    {      
+        // Désérialisation des données du corps de la requête en un objet User  
         $user = $serializer->deserialize($request->getContent(), User::class, "json");
+        // Récupération de l'utilisateur depuis la base de données
         $storedUser = $userRepository->findOneBy(['email' => $user->getEmail()]);
+        
         if (!$storedUser || !password_verify($user->getPassword(), $storedUser->getPassword())) {
             return new JsonResponse(['message' => 'Identifiants incorrects'], Response::HTTP_UNAUTHORIZED);
         }
-
+        // Vérification de l'activation de l'API
         if ($user->isApiEnabled()) {
             // Génération du token JWT
             $token = $this->jwtEncoder->encode([
@@ -79,7 +66,7 @@ class ApiLoginController extends AbstractController
             return new JsonResponse(['token' => $token]);
         } else {
             // Si l'accès à l'API est désactivé, retourner une réponse avec le statut 403 (Forbidden)
-            return new JsonResponse(['message' => 'Accès API non activé'], JsonResponse::HTTP_FORBIDDEN);
+            return new JsonResponse(['message' => 'API non active'], JsonResponse::HTTP_FORBIDDEN);
         }
     }
 }
--- src/Controller/ApiProductController.php
+++ src/Controller/ApiProductController.php
@@ -2,11 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
 use OpenApi\Attributes as OA;
 use App\Repository\ProductRepository;
-use Nelmio\ApiDocBundle\Annotation\Model;
-use Nelmio\ApiDocBundle\Annotation\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -16,14 +13,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ApiProductController extends AbstractController
 {
     #[Route('/api/products', name: 'products', methods: ['GET'])]
-    #[Security(name: 'Bearer')]
+    #[OA\Get(
+        summary: "Permet d'obtenir la liste des produits.",
+        description: "Détail des produits de Green Goodies",
+    )]
     #[OA\Response(
         response: 200,
         description: 'Retourne la liste des produits',
-        // content: new OA\JsonContent(
-        //     type: 'array',
-        //     items: new OA\Items(ref: new Model(type: Product::class))
-        // )
     )]
 
     #[OA\Tag(name: 'Products')]
@@ -32,7 +28,6 @@ class ApiProductController extends AbstractController
         $products = $productRepository->findAll();
 
         $jsonProducts = $serializer->serialize($products, 'json', ['groups' => 'getProduct']);
-        // $jsonProducts = $serializer->serialize($products, 'json');
 
         return new JsonResponse($jsonProducts, Response::HTTP_OK, [], true);
     }
